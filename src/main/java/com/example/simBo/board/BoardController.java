f1ab811ff949f1f65e735d8b082b0cb3ed40e90d
GH <mintaka04orion@naver.com>
2023-01-04 00:10:16 +0900

    폼 적용

--- src/main/java/com/example/simBo/board/BoardController.java
+++ src/main/java/com/example/simBo/board/BoardController.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.simBo.comment.CommentForm;
+
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RequestMapping("/board")
@@ -27,20 +30,25 @@ public class BoardController {
 	}
 	
 	@GetMapping(value = "/detail/{boardNum}")
-	public String detail(Model model, @PathVariable("boardNum") Integer num) {
+	public String detail(Model model, @PathVariable("boardNum") Integer num, 
+							CommentForm commentForm) {
 		Board board = this.boardService.getBoard(num);
 		model.addAttribute("board", board);
 		return "board_detail";
 	}
 	
 	@GetMapping("/create")
-	public String boardCreate() {
+	public String boardCreate(BoardForm boardForm) {
 		return "board_form";
 	}
 	
 	@PostMapping("/create")
-	public String boardCreate(@RequestParam String title, @RequestParam String content) {
-		this.boardService.create(title, content);
+	public String boardCreate(@Valid BoardForm boardForm, BindingResult bindingResult) {
+		
+		if(bindingResult.hasErrors()) {
+			return "board_form";
+		}
+		this.boardService.create(boardForm.getTitle(), boardForm.getContent());
 		return "redirect:/board/list";
 	}
 	
--- src/main/java/com/example/simBo/comment/CommentController.java
+++ src/main/java/com/example/simBo/comment/CommentController.java
@@ -2,6 +2,7 @@ package com.example.simBo.comment;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.example.simBo.board.Board;
 import com.example.simBo.board.BoardService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RequestMapping("/comment")
@@ -22,9 +24,17 @@ public class CommentController {
 	
 	@PostMapping("/create/{commentNum}")
 	public String createComment(Model model, @PathVariable("commentNum") Integer id,
-			@RequestParam String content) {
+			@RequestParam String content, 
+			@Valid CommentForm commentForm, BindingResult bindingResult) {
+		
 		Board board = this.boardService.getBoard(id);
-		this.commentService.create(board, content);
+
+		if(bindingResult.hasErrors()) {
+			model.addAttribute("board", board);
+			return "board_detail";
+		}
+		
+		this.commentService.create(board, commentForm.getContent());
 		return String.format("redirect:/board/detail/%s", id);
 	}
 	
